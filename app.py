ee3a9e1d1cea9298c162be97dec8f07e48df5884
sarahlyne46 <86782977+sarahlyne46@users.noreply.github.com>
2021-11-10 09:43:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h2 style='text-align: center; color: purple;'>Hello World! My name is Sarah and this is my Cloud DevOps Engineer Capstone Project!</h2>"
+@app.route("/")
+def blue():
+    html = https://sl-capstone-website.s3.amazonaws.com/bluedeploy/index.html 
+    return html
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+@app.route("/")
+def green():
+    html = https://sl-capstone-website.s3.amazonaws.com/greendeploy/index.html 
+    return html
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80    
