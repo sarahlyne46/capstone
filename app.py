3313436a4ff3f4f2f181e03d2562f7aac8122415
sarahlyne46 <86782977+sarahlyne46@users.noreply.github.com>
2021-11-15 11:04:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def blue():
-    html = "https://sl-capstone-website.s3.amazonaws.com/bluedeploy/index.html"
-    return html
-
-@app.route("/")
-def green():
-    html = "https://sl-capstone-website.s3.amazonaws.com/greendeploy/index.html"
-    return html
+def home():
+    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
