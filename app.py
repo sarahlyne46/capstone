92f7649e654ef69322089d4c706614f8b47afe60
EC2 Default User <ec2-user@ip-172-31-16-230.us-east-2.compute.internal>
2021-11-22 11:26:36 +0000

    adding new files

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project - 18th November!</h1>"
-
+    html = "<h3>This is a test for Capstone project</h3>"
+    return html.format(format)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
