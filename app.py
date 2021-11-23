cf5bfbf6f89608442b3d03e8cede059f264bbff4
EC2 Default User <ec2-user@ip-172-31-16-230.us-east-2.compute.internal>
2021-11-23 09:25:45 +0000

    adding new files

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<html><head style="background-color: lightblue; font-family:  verdana; color: purple;">Udacity Capstone Project</head><body style="background-color: lightpurple; font-family:  verdana; color: blue;">Capstone Project</body></html>'
-    
+    return "Welcome, My name is Sarah, This is Udacity Final project initial deployment"
+
 app.run(host="0.0.0.0", port=8080, debug=True)
