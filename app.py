e8d8d188248b10e25aee44ea22bebe683c4ad349
EC2 Default User <ec2-user@ip-172-31-16-230.us-east-2.compute.internal>
2021-11-23 09:45:42 +0000

    adding new files

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome, My name is Sarah, This is Udacity Final project initial deployment"
+    return "Welcome, My name is Sarah, This is Udacity Final project initial deployment version 2"
 
 app.run(host="0.0.0.0", port=8080, debug=True)
