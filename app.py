f97184b41a420e5c654fdec5c2d3ffbe3d509e11
sarahlyne46 <86782977+sarahlyne46@users.noreply.github.com>
2021-11-30 08:45:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ def index():
     </head>
     <body>
     <h1>Hello World!</h1>
-    <h2>My name is Sarah Lyne</h2>
-    <p> This is my blue deployment for the Capstone Project</p>
     </body>
     </html>
     """
